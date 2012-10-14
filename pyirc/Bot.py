97676db437fcfd0b73aa991114c1e6e5a4f70360
Chad Waters <authorblues@gmail.com>
2012-10-14 15:01:42 -0400

    Initial commit of pyirc framework

---
+++ pyirc/Bot.py
@@ -0,0 +1,198 @@
+# adapted from github.com/epw/pyirc
+import os, sys
+import imp
+import socket, select
+import shlex
+import time
+
+from collections import deque
+
+import pyirc.Message, pyirc.Plugin
+
+class Bot:
+	def changeNick(self, nick):
+		self.send('NICK {nick:s}'.format(nick=nick))
+		self.nick = nick
+		return self
+
+	def join(self, *chans):
+		for chan in chans:
+			self.joinq.append(chan)
+		return self
+
+	def part(self, *chans):
+		for chan in chans:
+			self.send('PART {:s}'.format(chan))
+			del self.channels[chan]
+		return self
+
+	def quit(self, reason=None):
+		rtext = '' if reason is None else ':{:s}'.format(reason)
+		self._sendImmediate('QUIT {:s}'.format(rtext).strip())
+		return self
+
+	def send(self, msg):
+		self.mbfr.append(msg)
+		return self
+
+	def _sendImmediate(self, msg):
+		if self.debug: print('<-- {:s}'.format(msg))
+		self.conn.send((msg + '\r\n').encode('utf8'))
+
+	def say(self, recp, msg):
+		self.send('PRIVMSG {recp:s} :{msg:s}'.format(recp=recp, msg=msg))
+		for p in self.plugins:
+			p.handleChat(recp, self.nick, msg)
+		return self
+
+	def sayTo(self, recp, target, msg):
+		self.say(recp, '{:s}: {:s}'.format(target, msg))
+		return self
+
+	def getChannels(self):
+		return self.channels.keys()
+
+	def __init__(self, nick, host, port=6667, passwd=None, real=None, debug=False):
+		self.mbfr = deque()
+		self.joinq = deque()
+		self.debug = debug
+
+		self.connected = False
+		self.authenticated = False
+
+		self.host, self.port, self.conn = host, port, None
+		while self.conn is None:
+			try:
+				print('Creating connection...')
+				self.conn = socket.create_connection((host, port), 0.5)
+			except socket.timeout as e:
+				self.conn = None
+
+		self.conn.setblocking(0)
+
+		self.real = real if real is not None else nick
+		self.passwd = passwd
+
+		self.send('PASS {passwd:s}'.format(passwd=passwd)).changeNick(nick)
+		self.send('USER {nick:s} {host:s} * :{real:s}'.format(nick=nick, host=host, real=real))
+
+		self.channels = {}
+		self.plugins = set()
+
+	def authenticate(self):
+		if self.passwd is not None:
+			params = {'nick': self.nick, 'passwd': self.passwd}
+			if 'quakenet' in self.host:
+				self.say('Q@CServe.quakenet.org', 'AUTH {nick:s} {passwd:s}'.format(**params))
+			elif 'gamesurge' in self.host:
+				self.say('AuthServ@Services.GameSurge.net', 'AUTH {nick:s} {passwd:s}'.format(**params))
+			else:
+				self.say('nickserv', 'GHOST {nick:s} {passwd:s}'.format(**params))
+		self.authenticated = True
+		return self
+
+	def loadPlugins(self, path):
+		for root, dirs, files in os.walk(path):
+			for name in files:
+				if name.endswith('.py') and not name.startswith('__'):
+					path = os.path.relpath(os.path.join(root, name))
+					mname = path[:-3].replace('/', '.')
+
+					mod = __import__(mname).__dict__
+					for v in mname.split('.')[1:]:
+						mod = mod[v].__dict__
+					for p in mod.values():
+						if p in pyirc.Plugin.Plugin.__subclasses__():
+							self.addPlugin(p(self))
+		return self
+
+	def addPlugin(self, plugin):
+		print('* Loading {:s}'.format(plugin.getPluginName()))
+		self.plugins.add(plugin)
+
+	def listen(self):
+		try:
+			while True:
+				# if any messages are ready to be listened to, listen to them
+				rlist, wlist, xlist = select.select([self.conn], [], [], 0.1)
+				for r in rlist: self.read(r)
+
+				if self.connected:
+					if not self.authenticated:
+						self.authenticate()
+					while len(self.joinq):
+						chan = self.joinq.popleft()
+					
+						if chan not in self.channels.values():
+							self.send('JOIN {chan:s}'.format(chan=chan))
+							self.channels[chan] = set()
+
+					clock = time.time()
+					for p in self.plugins:
+						if p.idledelay is not None and clock > p.idleclock + p.idledelay:
+							p.idleclock = clock
+							p.idle()
+
+				# send queued messages
+				while len(self.mbfr):
+					self._sendImmediate(self.mbfr.popleft())
+
+		# don't make a bit fuss about exiting
+		except (KeyboardInterrupt, SystemExit) as e:
+			self.quit(reason='Exiting')
+
+	def read(self, conn):
+		msg = ''
+		try:
+			while True: msg += conn.recv(1024).decode('utf8')
+		except Exception as e: pass
+
+		for m in msg.rstrip().split('\n'):
+			self.handle(pyirc.Message.Message(m))
+	   
+	def handle(self, msg):
+		if self.debug: print('--> {:s}'.format(msg))
+		if msg.isPing():
+			self._sendImmediate(msg.getPong())
+		else:
+			body = msg.getMessageText()
+			chan, nick, subnet = msg.getDelivery()
+			
+			if msg.msgtype == '001':
+				print('Connection established.')
+				self.connected = True
+
+			elif msg.msgtype == 'PRIVMSG':
+				for p in self.plugins:
+					p.handleChat(chan, nick, body)
+
+				if body.startswith(pyirc.Message.commandChar):
+					cmd, *args = shlex.split(body[1:])
+					for p in self.plugins:
+						p.handleCommand(chan, nick, cmd, args)
+
+			elif msg.msgtype == 'JOIN':
+				for p in self.plugins:
+					p.onChannelJoin(chan, nick)
+
+			elif msg.msgtype == 'PART':
+				for p in self.plugins:
+					p.onChannelPart(chan, nick)
+
+			elif msg.msgtype == 'QUIT':
+				for p in self.plugins:
+					p.onQuit(chan, nick, reason=body)
+
+			elif msg.msgtype == 'NICK':
+				for p in self.plugins:
+					p.onNickChange(nick, msg.get(2))
+
+			elif msg.msgtype == 'KICK':
+				for p in self.plugins:
+					p.onQuit(chan, msg.get(3), reason=body)
+
+			elif msg.msgtype == 'INVITE':
+				if self.nick == msg.get(2):
+					for p in self.plugins:
+						p.onInvite(msg.get(3))
+					
---
+++ pyirc/Plugin.py
@@ -0,0 +1,51 @@
+import time, datetime
+
+class Plugin:
+	def __init__(self, bot):
+		self.idledelay = None
+		self.bot = bot
+	
+	def handleChat(self, chan, sender, msg):
+		return False
+
+	def handlePrivateMessage(self, sender, msg):
+		return self.handleChat(sender, sender, msg)
+
+	def handleCommand(self, chan, sender, cmd, args):
+		return False
+
+	def onChannelJoin(self, chan, nick):
+		return False
+
+	def onChannelPart(self, chan, nick):
+		return False
+
+	def onQuit(self, nick, reason=None):
+		return False
+
+	def onInvite(self, chan):
+		return False
+
+	def onKick(self, chan, nick, reason=None):
+		return False
+
+	def onNickChange(self, oldnick, newnick):
+		return False
+
+	def onModeChange(self, chan, nick):
+		return False
+
+	def idle(self):
+		return False
+
+	def setIdleTimer(self, sec=None):
+		self.idledelay = sec
+		self.idleclock = time.time()
+
+	def getPluginName(self):
+		return self.__class__.__name__
+
+	def log(self, message):
+		now = datetime.datetime.now()
+		now -= datetime.timedelta(microseconds=now.microsecond)
+		print('[{:s}] <{:s}> {:s}'.format(str(now), self.getPluginName(), message))
