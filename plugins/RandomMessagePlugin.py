a964e2bf899b9f5a18820455e09f10e3f8cee502
Chad Waters <authorblues@gmail.com>
2012-10-17 23:51:15 -0400

    Modify random message plugin to lessen the noise in an inactive channel

--- plugins/RandomMessagePlugin.py
+++ plugins/RandomMessagePlugin.py
@@ -2,8 +2,9 @@
 RandomMessagePlugin by caske33
 
 config:
+	message-threshold = number of messages that must be sent between random messages
 	delay-minutes = number of minutes between messages
-    message-file [default: messages.txt] = file with messages, one per line 
+	message-file [default: messages.txt] = file with messages, one per line 
 """
 
 import os, random
@@ -11,10 +12,17 @@ import os, random
 import pyirc.Plugin
 
 class RandomMessagePlugin(pyirc.Plugin.Plugin):
-    def __init__(self, bot):
-        super().__init__(bot)
-        self.setIdleTimer(60.0*int(self.getConfig('delay-minutes')))
+	def __init__(self, bot):
+		super().__init__(bot)
+		self.setIdleTimer(60.0*int(self.getConfig('delay-minutes')))
+		self.messageCount = 0
+		self.messageThreshold = int(self.getConfig('message-threshold', 5))
 
-    def idle(self):
-        with open(self.getConfig('message-file', 'messages.txt'), 'r') as f:
-            self.bot.sayAll(random.choice(f.readlines()))
+	def handleChat(self, chan, sender, msg):
+		self.messageCount += 1
+
+	def idle(self):
+		if self.messageCount < self.messageThreshold: return
+		self.messageCount = 0
+		with open(self.getConfig('message-file', 'messages.txt'), 'r') as f:
+			self.bot.sayAll(random.choice(f.readlines()))
